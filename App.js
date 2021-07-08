bedcf2c47e5f20f96461b264773370f003bc1456
Nguyễn Trung Dũng - CMCGlobal DE1 <ntdung5@ldap.cmcglobal.com.vn>
2021-07-08 15:06:21 +0700

    init-project-react-native

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Example from './component/Example'
+
+const App = () => { 
+  return (
+   <Example/>
+  );
+};
+
+export default App;
